ed90f4cdcd776fe0b7cecffc6282eabbea322f4b
Alessandro Holanda <alessandro.holanda@hotmail.com>
2016-12-29 16:56:47 -0200

    initial commit

---
+++ src/main/java/br/com/altamira/manufatura/App.java
@@ -0,0 +1,9 @@
+package br.com.altamira.manufatura;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication
+public class App extends ServletProcessApplication {
+  // empty implementation
+}
